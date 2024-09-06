e1edb7b73d42dc40935e57d44a05475767642494
durgesh-firake09 <firake.durgesh876@gmail.com>
2024-09-06 22:35:56 +0530

    deploy-1

--- blog/settings.py
+++ blog/settings.py
@@ -21,12 +21,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-6hrsxd&$1tk0ub(&4aphpgs^164sh#hk^5#oh4&b2d4&9$q@vh'
+# SECRET_KEY = 'django-insecure-6hrsxd&$1tk0ub(&4aphpgs^164sh#hk^5#oh4&b2d4&9$q@vh'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get("DEBUG",'False').lower() == 'true'
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS").split(" ")
 
 
 # Application definition
@@ -131,5 +132,5 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
 ]
 
-
-DATABASES['default'] = dj_database_url.parse("postgresql://durgesh:DPW0DvJwt5jXIRRPldqeRiHmxYYh2JNr@dpg-crdiujl2ng1s73802qfg-a.oregon-postgres.render.com/blog_zsy0")
+database_url = os.environ.get("DATABASE_URL")
+DATABASES['default'] = dj_database_url.parse(database_url)
